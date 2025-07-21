c7cbd23553b0d85c146389ea8277a4294d750fe7
menuvercel2 <menuvercel2@gmail.com>
2025-07-20 21:54:43 -0400

    cambio de numero

--- components/cart-dialog.tsx
+++ components/cart-dialog.tsx
@@ -118,7 +118,7 @@ ${itemsList}
 `
 
     const encodedMessage = encodeURIComponent(message)
-    const whatsappUrl = `https://wa.me/5354547503?text=${encodedMessage}`
+    const whatsappUrl = `https://wa.me/5350540189?text=${encodedMessage}`
 
     window.open(whatsappUrl, "_blank")
 
